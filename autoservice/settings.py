eb921f2eb0961b17918b7481c0c627e1da89b026
Miras <mirasprog@gmail.com>
2024-04-04 14:56:20 +0500

    Обновил ссылки и настройки, дабовил дату в футер

--- autoservice/settings.py
+++ autoservice/settings.py
@@ -114,7 +114,7 @@ LANGUAGE_CODE = 'ru-RU'
 
 TIME_ZONE = 'Asia/Almaty'
 
-USE_I18N = False
+USE_I18N = True
 
 USE_TZ = True
 
--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('name/', views.name, name='author_name'),
-    path('project/', views.project, name='project_info')
+    re_path('name/', views.name, name='author_name'),
+    re_path('project/', views.project, name='project_info')
 ]

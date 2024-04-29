938f9df7b19fe45d77e5659231418207cad1e7a4
Kantaev Sherhan <kansherhan@gmail.com>
2024-04-29 18:54:47 +0500

    added routing

--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,11 @@
-from django.urls import path, re_path
+from django.urls import path
 
-from . import views
 from main import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    # re_path('name/', views.name, name='author_name'),
-    # re_path('project/', views.project, name='project_info'),
+    path('name/', views.name, name='author_name'),
+    path('project/', views.project, name='project_info'),
     path('api/', views.ServiceList.as_view(), name='service_list'),
     path('api/<int:pk>/', views.ServiceList2.as_view(), name='service_list2')
 ]
